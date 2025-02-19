722e392b3204ef5e23076cfe4d19bbcbfb138925
V <mr.vaavee@gmail.com>
2025-02-19 21:30:58 +0400

    the commit

---
+++ Main.py
@@ -0,0 +1,11 @@
+print ("welcome to pass reminder")
+
+inAnimal = input ("Enter your favorite Animal: ")
+inName = input ("Enter your Name: ")
+inAge = input ("Enter your Age: ")
+
+#print (inAnimal, inName, inAge)
+
+password = inAnimal + inName + inAge
+
+print (password)
